f72eb8211aeaac6b9b305e25164a05660ca4390b
NguyenMinhtrieu <trieunguyen912@.com>
2023-05-20 10:05:40 +0700

    update code

---
+++ src/data/statistics-cards-data.js
@@ -0,0 +1,24 @@
+import {
+  BanknotesIcon,
+  UserPlusIcon,
+  BeakerIcon,
+
+  ChartBarIcon,
+} from "@heroicons/react/24/solid";
+
+export const statisticsCardsData = [
+
+  {
+    color: "orange",
+    icon: ChartBarIcon,
+    title: "Nhận xét",
+    value: "Sức chịu tải của thành cọc giảm 7,9 %  Khi có sự ảnh hưởng của hóa lỏng , Sức chịu tải cực hạn mũi cọc giảm 8,48 %  Khi có sự ảnh hưởng của hóa lỏng",
+    footer: {
+      color: "text-green-500",
+      value: "Tổng sức chịu tải của cọc giảm ",
+      label: "7,82 %",
+    },
+  },
+];
+
+export default statisticsCardsData;
